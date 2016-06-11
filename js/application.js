852117af1cddfdcc2778272d05afa0093a87d432
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-06-11 12:45:54 +0200

    Embed brainbow in presentation

--- js/application.js
+++ js/application.js
@@ -1,5 +1,19 @@
 (function(Reveal){
+    function once(callback) {
+        var count = 0;
+        return function(){
+            if (count++ === 0) {
+                callback();
+            }
+        };
+    }
+
     Reveal.addEventListener('ready', function(){
         console.log('Reveal is ready to rock');
     });
+
+    Reveal.addEventListener('brainbow', once(function(){
+        var container = document.getElementById('brainbow-container');
+        var app = Elm.Main.embed(container);
+    }));
 })(Reveal);
